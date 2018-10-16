842993359b47ea472b0756518685266757f40583
David Fox <dsf@seereason.com>
2018-10-16 07:28:25 -0700

    Update test cases

--- tests/Main.hs
+++ tests/Main.hs
@@ -42,7 +42,7 @@ tests = TestList
                   $(safeCopyInstance 1 'base [t|SerializedIndex|] >>= lift . pprint1))
     , TestCase (assertEqual "SafeCopy Hop"
                   (mconcat
-                   ["instance (SafeCopy key, Typeable key) => SafeCopy (Hop key) where",
+                   ["instance SafeCopy key => SafeCopy (Hop key) where",
                     " putCopy (FieldHop a1 a2) = contain (do {putWord8 0; safePut_Int <- getSafePut; safePut_Int a1; safePut_Int a2; return ()})",
                     " putCopy (IxHop a1) = contain (do {putWord8 1; safePut_key <- getSafePut; safePut_key a1; return ()})",
                     " putCopy (AtHop a1) = contain (do {putWord8 2; safePut_key <- getSafePut; safePut_key a1; return ()})",
@@ -54,7 +54,7 @@ tests = TestList
                   $(safeCopyInstance 1 'base [t|Hop|] >>= lift . pprint1))
     , TestCase (assertEqual "SafeCopy TypePath"
                   (mconcat
-                   ["instance (Typeable t1, Typeable s1, Typeable a1) => SafeCopy (Path t1 (Proxy s1) (Proxy a1)) where",
+                   ["instance SafeCopy (Path t1 (Proxy s1) (Proxy a1)) where",
                     " putCopy (Path a1 a2 a3) = contain (do {safePut_Proxys1 <- getSafePut; safePut_ListHopSerializedIndex <- getSafePut; safePut_Proxya1 <- getSafePut; safePut_Proxys1 a1; safePut_ListHopSerializedIndex a2; safePut_Proxya1 a3; return ()})",
                     " getCopy = contain (label \"TypePath t1 s1 a1:\" (do {safeGet_Proxys1 <- getSafeGet; safeGet_ListHopSerializedIndex <- getSafeGet; safeGet_Proxya1 <- getSafeGet; ((return Path <*> safeGet_Proxys1) <*> safeGet_ListHopSerializedIndex) <*> safeGet_Proxya1}))",
                     " version = 1",
@@ -63,7 +63,7 @@ tests = TestList
                   $(safeCopyInstance 1 'base [t|TypePath|] >>= lift . pprint1))
     , TestCase (assertEqual "SafeCopy TypeSPath"
                   (mconcat
-                   ["instance (Typeable t1, Typeable s1) => SafeCopy (Path t1 (Proxy s1) ()) where",
+                   ["instance SafeCopy (Path t1 (Proxy s1) ()) where",
                     " putCopy (Path a1 a2 a3) = contain (do {safePut_Proxys1 <- getSafePut; safePut_ListHopSerializedIndex <- getSafePut; safePut_Tuple0 <- getSafePut; safePut_Proxys1 a1; safePut_ListHopSerializedIndex a2; safePut_Tuple0 a3; return ()})",
                     " getCopy = contain (label \"TypeSPath t1 s1:\" (do {safeGet_Proxys1 <- getSafeGet; safeGet_ListHopSerializedIndex <- getSafeGet; safeGet_Tuple0 <- getSafeGet; ((return Path <*> safeGet_Proxys1) <*> safeGet_ListHopSerializedIndex) <*> safeGet_Tuple0}))",
                     " version = 1",
@@ -72,7 +72,7 @@ tests = TestList
                   $(safeCopyInstance 1 'base [t|TypeSPath|] >>= lift . pprint1))
     , TestCase (assertEqual "SafeCopy PathValue"
                  (mconcat
-                   ["instance (Typeable t3, Typeable s3) => SafeCopy (PathValue t3 s3) where",
+                   ["instance SafeCopy (PathValue t3 s3) where",
                     " putCopy (PathValue a1 a2) = contain (do {safePut_Patht1Proxys1Tuple0t3s3 <- getSafePut; safePut_ByteString <- getSafePut; safePut_Patht1Proxys1Tuple0t3s3 a1; safePut_ByteString a2; return ()})",
                     " getCopy = contain (label \"PathValue t3 s3:\" (do {safeGet_Patht1Proxys1Tuple0t3s3 <- getSafeGet; safeGet_ByteString <- getSafeGet; (return PathValue <*> safeGet_Patht1Proxys1Tuple0t3s3) <*> safeGet_ByteString}))",
                     " version = 1",
@@ -81,7 +81,7 @@ tests = TestList
                   $(safeCopyInstance 1 'base [t|PathValue|] >>= lift . pprint1))
     , TestCase (assertEqual "SafeCopy Either"
                   (mconcat
-                   ["instance (SafeCopy k, SafeCopy a, Typeable k, Typeable a) => SafeCopy (Map k a) where",
+                   ["instance (SafeCopy k, SafeCopy a) => SafeCopy (Map k a) where",
                     " putCopy (Bin a1 a2 a3 a4 a5) = contain (do {putWord8 0; safePut_Int <- getSafePut; safePut_k <- getSafePut; safePut_a <- getSafePut; safePut_Mapka <- getSafePut; safePut_Int a1; safePut_k a2; safePut_a a3; safePut_Mapka a4; safePut_Mapka a5; return ()})",
                     " putCopy (Tip) = contain (do {putWord8 1; return ()})",
                     " getCopy = contain (label \"Map k a:\" (do {tag <- getWord8; case tag of 0 -> do {safeGet_Int <- getSafeGet; safeGet_k <- getSafeGet; safeGet_a <- getSafeGet; safeGet_Mapka <- getSafeGet; ((((return Bin <*> safeGet_Int) <*> safeGet_k) <*> safeGet_a) <*> safeGet_Mapka) <*> safeGet_Mapka} 1 -> do return Tip _ -> fail (\"Could not identify tag \\\"\" ++ (show tag ++ \"\\\" for type \\\"Map k a\\\" that has only 2 constructors.  Maybe your data is corrupted?\"))}))",
